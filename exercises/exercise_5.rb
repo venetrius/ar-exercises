cca71a592a7ccf3dfff38c865ab9a2f70d7a63a6
venetrius <gergelygjuhasz@gmail.com>
2019-06-11 17:37:00 +0000

    excercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+sum_of_annual_revenues = Store.sum(:annual_revenue)
+average_annual_revenue = sum_of_annual_revenues / Store.count
+puts "sum of annual revenues : #{sum_of_annual_revenues}"
+puts "the average annual revenue : #{average_annual_revenue}"
