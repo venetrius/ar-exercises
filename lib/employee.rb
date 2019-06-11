278642377557da15f57ed82ad4cb96a569884dc0
venetrius <gergelygjuhasz@gmail.com>
2019-06-11 19:40:44 +0000

    add "before_validation" callback to "employee" which generates "password"

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,6 +1,8 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
 
+  before_validation :add_generated_password
+
   validates :store_id, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
@@ -9,5 +11,10 @@ class Employee < ActiveRecord::Base
     greater_than_or_equal_to: 40,
     less_than_or_equal_to:    200
   }
+
+  private
+    def add_generated_password
+      self.password = ('a'..'z').to_a.shuffle[0,8].join
+    end
 
 end
