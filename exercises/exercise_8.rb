a19742b7086e6f169be0cf073e3d10a6d78d9507
venetrius <gergelygjuhasz@gmail.com>
2019-06-11 19:42:27 +0000

    verify "before_validation"

--- exercises/exercise_8.rb
+++ exercises/exercise_8.rb
@@ -6,3 +6,9 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 require_relative './exercise_7'
+
+puts "Exercise 8"
+puts "----------"
+
+new_one = @store1.employees.create(first_name: "newOne", last_name: "Bator", hourly_rate: 62)
+puts new_one.password
