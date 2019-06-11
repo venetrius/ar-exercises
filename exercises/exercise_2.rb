13f8e6e4abd036a2d71b8af314b26c9199d76002
venetrius <gergelygjuhasz@gmail.com>
2019-06-11 17:01:20 +0000

    excercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,8 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(:id => 1);
+@store2 = Store.find_by(:id => 2);
+puts @store1.name
+@store1.name = 'Euler'
+puts @store1.name
