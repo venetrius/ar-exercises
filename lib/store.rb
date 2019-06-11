f2fbaeaa85a4f33abf7b93ab31d5d124765892c5
venetrius <gergelygjuhasz@gmail.com>
2019-06-11 19:56:22 +0000

    Exercise 9: Disallow store destruction where store has employee

--- lib/store.rb
+++ lib/store.rb
@@ -3,6 +3,14 @@ class Store < ActiveRecord::Base
   validates :name, length: { minimum: 3}
   validates :annual_revenue, numericality: { 
     only_integer:             true,
-    greater_than_or_equal_to: 0
+    greater_than_or_equal_to: 0, prepend: true
   }
+  before_destroy :ensure_no_employees
+
+  def ensure_no_employees
+    if self.employees.any?
+      errors[:base] << "cannot delete a store which has one or more employees"
+      throw :abort
+    end
+  end
 end
